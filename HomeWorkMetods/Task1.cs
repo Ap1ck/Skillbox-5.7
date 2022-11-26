891ffc256cdfbf9b2b6fd6cdf0f835ad8550f291
Alexandr Popov <He1ton@yandex.ru>
2022-11-26 21:10:49 +0300

    HomeWork

---
+++ HomeWorkMetods/Task1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWorkMetods
+{
+    class Task1
+    {
+        public static void Main(string[] args)
+        {
+            SplitText(InputLine());
+            Console.ReadLine();
+        }
+
+        private static string[] SplitText(string text)
+        {
+            string[] allText = { text };
+            allText = text.Split(' ');
+
+            foreach (var findText in allText)
+            {
+                Console.WriteLine($"Sybstring: {findText}");
+            }
+
+            return allText;
+        }
+
+        private static string InputLine()
+        {
+            Console.Write("Введите строку: ");
+            string line = Console.ReadLine();
+            return line;
+        }
+    }
+}
---
+++ HomeWorkMetods/Task2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWorkMetods
+{
+    class Task2
+    {
+        public static void Main()
+        {
+            RewersWords(Reverse());
+            Console.ReadLine();
+        }
+
+        public static string RewersWords(string inputPhrase)
+        {
+            string[] allText = { inputPhrase };
+            allText = inputPhrase.Split(' ');
+
+            return inputPhrase;
+        }
+
+        public static string Reverse()
+        {
+            Console.Write("Введите текст: ");
+            var words = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(string.Join(" ", words.Reverse()));
+            return RewersWords(Reverse());
+        }
+    }
+}
